d8a0b68e421c049cc681d74febab6fe139114980
Amanda Kirchner <geeky.brewster@gmail.com>
2017-08-08 17:34:09 -0500

    Polished layout more. Added a few more images, including API credit on login.

---
+++ server/public/scripts/controllers/message.controller.js
@@ -0,0 +1,12 @@
+//BUILD A FUNCTION TO RANDOMLY GENERATE A 'CHEERS' MESSAGE FOR THE HEADER
+var cheers = [
+  'Prost',
+  'Salud',
+  'Cheers',
+  'Kanpai',
+  'Salut',
+  'Sei gesund',
+  'Budem zdorovi',
+  'Gun bae',
+  'Yamas'
+];
