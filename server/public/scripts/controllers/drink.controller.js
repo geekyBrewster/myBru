311497d18138fd334970ca70a1fa978a23f102cc
Amanda Kirchner <geeky.brewster@gmail.com>
2017-08-08 15:23:47 -0500

    finalProduct.html mostly complete. badBeer partial layout improved.

--- server/public/scripts/controllers/drink.controller.js
+++ server/public/scripts/controllers/drink.controller.js
@@ -1,6 +1,15 @@
-myApp.controller('DrinkController', function(UserService) {
+myApp.controller('DrinkController', function(UserService, $scope) {
   console.log('DrinkController created');
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
+
+  $scope.offFlavors = {
+    flavor: 'flavorBad',
+    description: 'someDescription'
+  };
+
+
+
+
 });
