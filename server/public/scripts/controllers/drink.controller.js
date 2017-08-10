dbc74fa3a50a0752a8a4f744bff622917fc3637f
Amanda Kirchner <geeky.brewster@gmail.com>
2017-08-10 13:42:14 -0500

    Receiving data from drink.html on the client side.

--- server/public/scripts/controllers/drink.controller.js
+++ server/public/scripts/controllers/drink.controller.js
@@ -4,20 +4,35 @@ myApp.controller('DrinkController', function(UserService, $scope, $mdDialog) {
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
 
-  vm.view = "views/partials/defaultImage.html";
+  //DATA OBJECTS
+  vm.recipe = {};
+  // vm.batch = recipe.batch;
+  vm.finalProduct = {};
 
+  vm.view = "views/partials/defaultImage.html";
+//TOGGLES NOTES FIELD DEPENDING ON RATING OF BEER
+    vm.toggleNotes = function(value){
+      if(!value){
+        vm.view = "views/partials/badBeer.html";
+        console.log('Switching to bad notes');
+      } else {
+        vm.view = "views/partials/goodBeer.html";
+        console.log('Switching to good notes');
+      }
+    };
+
+  //Open menu function
+    var originatorEv;
+    vm.openMenu = function($mdMenu, ev) {
+      originatorEv = ev;
+      $mdMenu.open(ev);
+    };
+
+//** OFF FLAVORS INPUT FUNCTIONS **//
   vm.offFlavors = offFlavorsDB;
   // console.log("Off flavors db: ", vm.offFlavors);
   vm.offFlavorDescription = "";
   vm.offFlavorArray = [];
-
-//** OFF FLAVORS INPUT FUNCTIONS **//
-  //Open menu function
-  var originatorEv;
-  vm.openMenu = function($mdMenu, ev) {
-    originatorEv = ev;
-    $mdMenu.open(ev);
-  };
 
   //LOADS DATA INTO OFF FLAVORS PULLDOWN MENU
     $scope.offFlavors = vm.offFlavors;
@@ -50,21 +65,30 @@ myApp.controller('DrinkController', function(UserService, $scope, $mdDialog) {
     //APPENDS TO DOM via offFlavorArray
   };
 
-
-//TOGGLES NOTES FIELD DEPENDING ON RATING OF BEER
-  vm.toggleNotes = function(value){
-    if(!value){
-      vm.view = "views/partials/badBeer.html";
-      console.log('Switching to bad notes');
-    } else {
-      vm.view = "views/partials/goodBeer.html";
-      console.log('Switching to good notes');
-    }
+//SAVE FINAL PRODUCT NOTES -- ON CLICK
+  vm.saveFinalProduct = function(date, rank, worthRepeating, impressions,
+    batchChanges, aroma, appearance, flavor, mouthfeel, batchNotes, suspectedCauses){
+    //BUILD DATA OBJECT TO SEND TO SERVER
+    vm.finalProduct = {
+      // fromBatchID: vm.batch.batchID,
+      fromBatchID: 1,
+      drinkDate: date,
+      batchRank: rank,
+      worthRepeating: worthRepeating,
+      batchChanges: batchChanges,
+      aroma: aroma,
+      appearance: appearance,
+      flavor: flavor,
+      mouthfeel: mouthfeel,
+      batchImpressions: impressions,
+      batchNote: batchNotes,
+      offFlavors: vm.offFlavorArray,
+      suspectedCauses: suspectedCauses
+    };
+    console.log('Drink data obj: ', vm.finalProduct);
+    //POST TO /drink to add drink[] to recipe{}
   };
 
-//SAVE FINAL PRODUCT NOTES -- ON CLICK
-  //BUILD DATA OBJECT TO SEND TO SERVER
-  //POST TO /drink to add drink[] to recipe{}
 
 
 
