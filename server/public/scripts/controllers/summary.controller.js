556f9d28352878e187edaf81141ce9466d25c4d2
Amanda Kirchner <geeky.brewster@gmail.com>
2017-08-16 12:06:50 -0500

    Added initial xeditable functionality to recipe specifics.

--- server/public/scripts/controllers/summary.controller.js
+++ server/public/scripts/controllers/summary.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('SummaryController', function($http, $location, UserService, RecipeService) {
+myApp.controller('SummaryController', function($http, $location, $scope, $q, UserService, RecipeService) {
   console.log('SummaryController created');
   var vm = this;
   vm.userService = UserService;
@@ -10,12 +10,46 @@ myApp.controller('SummaryController', function($http, $location, UserService, Re
   vm.recipeService.getAllRecipes();
   console.log(vm.allRecipes);
 
+  //xeditabel functions
+
+  // cancel all changes
+
+  //save edits
+  $scope.saveTable = function() {
+    var results = [];
+    for (var i = $scope.users.length; i--;) {
+      var user = $scope.users[i];
+      // actually delete user
+      if (user.isDeleted) {
+        $scope.users.splice(i, 1);
+      }
+      // mark as not new
+      if (user.isNew) {
+        user.isNew = false;
+      }
+
+      // send on server
+      results.push($http.post('/saveUser', user));
+    }
+
+    return $q.all(results);
+  };
+
+
   //PUT updated recipe data -- /update/recipe/id
+  vm.updateRecipe = function(){
+
+  };
 
   //PUT updated brew batch data  -- /update/brew/id
+  vm.updateBrew = function(){
+
+  };
 
   //PUT updated drink brew data  -- /update/drink/id
+  vm.updateDrink = function(){
 
+  };
 
 
 
