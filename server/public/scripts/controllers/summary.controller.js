04b74197f9f9f7d397c0dcf0fce4354375de8169
Amanda Kirchner <geeky.brewster@gmail.com>
2017-08-16 14:23:18 -0500

    Rearranging layout after adding updatable text areas.

--- server/public/scripts/controllers/summary.controller.js
+++ server/public/scripts/controllers/summary.controller.js
@@ -10,31 +10,6 @@ myApp.controller('SummaryController', function($http, $location, $scope, $q, Use
   vm.recipeService.getAllRecipes();
   console.log(vm.allRecipes);
 
-  //xeditabel functions
-
-  // cancel all changes
-
-  //save edits
-  $scope.saveTable = function() {
-    var results = [];
-    for (var i = $scope.users.length; i--;) {
-      var user = $scope.users[i];
-      // actually delete user
-      if (user.isDeleted) {
-        $scope.users.splice(i, 1);
-      }
-      // mark as not new
-      if (user.isNew) {
-        user.isNew = false;
-      }
-
-      // send on server
-      results.push($http.post('/saveUser', user));
-    }
-
-    return $q.all(results);
-  };
-
 
   //PUT updated recipe data -- /update/recipe/id
   vm.updateRecipe = function(){
